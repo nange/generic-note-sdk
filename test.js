cba3bc637f52755a26f0aabecc5f19eb0d556780
nange <463499510@qq.com>
2014-05-19 00:33:57 +0800

    update

--- test.js
+++ test.js
@@ -1,59 +1,80 @@
+/**
+ * Module dependencies.
+ */
 var express = require('express');
-var GenericNote = require('./GenericNote.js');
-var Evernote = require('evernote').Evernote;
+var GenericNote = require('./lib/GenericNote.js');
+var config = require('./config.js');
 
 var app = express();
 
 app.get('/list_notebook', function(req, res) {
-  var genNote = new GenericNote();
-  var noteStore = genNote.getNoteStore();
+  var genNote = GenericNote(config.devToken, config.SANDBOX, 'evernote');
 
-  noteStore.listNotebooks(function(err, noteBooks) {
+  genNote.listAllNotebooks(function(err, books) {
     if (err) {
       console.log(err);
       res.send('get noteBooks error');
       return;
     }
 
-    noteBooks.forEach(function(value, index) {
-      console.log('notebook name: ' + value.name);
+    books.forEach(function(value, index) {
+      console.log('notebook name: ' + value.name 
+        + ' notebook uid: ' + value.uid + '\n');
     });
 
     res.send('get noteBooks success');
+  });
+
+});
+
+app.get('/get_firstnote', function(req, res) {
+  var genNote = GenericNote(config.devToken, config.SANDBOX, 'evernote');
+
+  genNote.listAllNotebooks(function(err, books) {
+    if (err) {
+      console.log(err);
+      res.send('get noteBooks error');
+      return;
+    }
+
+    genNote.listNoteUidsFromBook(books[0].uid, function(err, noteUidsList) {
+      if (err) {
+        console.log(err);
+        res.send('get noteUids error');
+        return;
+      }
+
+      genNote.getNote(noteUidsList[0], function(err, note) {
+        if (err) {
+          console.log(err);
+          res.send('get note error');
+          return;
+        }
+
+        console.log('fist note title: ' + note.title + '\n');
+
+        res.send('get first note success');
+      });
+
+    });
 
   });
 
 });
 
-app.get('/list_note', function(req, res) {
-  var genNote = new GenericNote();
-  var noteStore = genNote.getNoteStore();
+app.get('/get_user', function(req, res) {
+  var genNote = GenericNote(config.devToken, config.SANDBOX, 'evernote');
 
-  noteStore.findNotesMetadata(new Evernote.NoteFilter(), 0, 10,
-    new Evernote.NotesMetadataResultSpec(), function(err, nml) {
+  genNote.getUser(function(err, user) {
     if (err) {
       console.log(err);
-      res.send('get notes error');
+      res.send('get user err');
       return;
     }
-
-    var notes = nml.notes;
-
-    notes.forEach(function(value, index) {
-      noteStore.getNote(value.guid, true, false, false, false,
-        function(err, note) {
-        if (err) {
-          console.log(err);
-          res.send('get notes err');
-          return;
-        }
-        console.log('note title: ' + note.title +
-          ' note content: ' + note.content + '\n');
-
-        if (index === notes.length - 1) res.send('get notes success');
-      });
-    });
-
+    console.log('userName: ' + user.userName 
+      + ' createdTime: ' + user.createdTime 
+      + ' updatedTime: ' + user.updatedTime + '\n');
+    res.send('get user success');
   });
 
 });
